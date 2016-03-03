1a27abe75ee5ed9663f12fe0d9c003ed29027b16
ipandya <ipandya.wpdev@outlook.com>
2016-03-03 22:43:11 +0530

    Removed unnecessary scripts

--- ShopAppTest-Database.sql
+++ ShopAppTest-Database.sql
@@ -1,5 +1,1 @@
-CREATE DATABASE ShoppingAppTestON  PRIMARY 
-( NAME = N'ShoppingAppTest', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL10.MSSQLSERVER\MSSQL\DATA\ShoppingAppTest.mdf' , SIZE = 3072KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
- LOG ON 
-( NAME = N'ShoppingAppTest_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL10.MSSQLSERVER\MSSQL\DATA\ShoppingAppTest_log.ldf' , SIZE = 1024KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
-GO
+CREATE DATABASE ShoppingAppTest
