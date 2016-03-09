f1be75e83844ddcea6ba3d9b4b83ac3eb6b6b9c4
ipandya <ipandya.wpdev@outlook.com>
2016-03-09 23:17:39 +0530

    Added script for customer table

--- Documents/ShopAppTest-Database.sql
+++ Documents/ShopAppTest-Database.sql
@@ -1,1 +1,16 @@
 CREATE DATABASE ShoppingAppTest
+
+CREATE TABLE CustomerMaster(
+	CustomerID numeric(18, 0) IDENTITY(1,1) NOT NULL,
+	CustomerTitle bit NOT NULL,
+	CustomerFName text NOT NULL,
+	CustomerLName nchar(10) NOT NULL,
+	[Address] text NULL,
+	ZipCode text NULL,
+	City text NOT NULL,
+	EmailAddress text NOT NULL,
+ CONSTRAINT PK_CustomerMaster PRIMARY KEY CLUSTERED 
+(
+	CustomerID ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
