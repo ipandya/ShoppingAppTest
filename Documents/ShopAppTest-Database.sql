cdfe31667eb6d138698aef35af20806537da2832
ipandya <ipandya.wpdev@outlook.com>
2016-03-09 23:19:07 +0530

    Added product master

--- Documents/ShopAppTest-Database.sql
+++ Documents/ShopAppTest-Database.sql
@@ -14,3 +14,20 @@ CREATE TABLE CustomerMaster(
 	CustomerID ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+
+
+CREATE TABLE ProductMaster(
+	ProductID numeric(18, 0) IDENTITY(1,1) NOT NULL,
+	ProductTitle varchar(80) NOT NULL,
+	ProductDescription varchar(200) NOT NULL,
+	ProductDescription2 text NULL,
+	Price numeric(18, 0) NOT NULL,
+	Discount numeric(18, 0) NULL,
+	Images text NULL,
+	Quantity numeric(18, 0) NOT NULL,
+ CONSTRAINT [PK_ProductMaster] PRIMARY KEY CLUSTERED 
+(
+	[ProductID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
